3dd28f1ae8c59e0444c30c0ce8691f315cd1dfb8
uji100 <97239721+uji100@users.noreply.github.com>
2023-08-01 01:35:27 +0530

    adding Hibernate annotations

--- Part-01/src/main/java/software/ujithamigara/entity/Book.java
+++ Part-01/src/main/java/software/ujithamigara/entity/Book.java
@@ -1,6 +1,14 @@
 package software.ujithamigara.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String ISBN;
