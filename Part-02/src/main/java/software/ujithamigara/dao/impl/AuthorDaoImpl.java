14525f752cd8c275d2ada57c45b5585b73f3d9ba
uji100 <97239721+uji100@users.noreply.github.com>
2023-08-02 13:10:36 +0530

    add AuthorDaoImpl entity

---
+++ Part-02/src/main/java/software/ujithamigara/dao/impl/AuthorDaoImpl.java
@@ -0,0 +1,26 @@
+package software.ujithamigara.dao.impl;
+
+import software.ujithamigara.dao.AuthorDao;
+import software.ujithamigara.entity.Author;
+
+public class AuthorDaoImpl implements AuthorDao {
+    @Override
+    public boolean saveAuthor(Author author) {
+        return false;
+    }
+
+    @Override
+    public Author searchBook(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean updateBook(Author author) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteBook(Author author) {
+        return false;
+    }
+}
