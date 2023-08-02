f5f8352f160a926dfec91e5e63aeb03cfa4c34cd
uji100 <97239721+uji100@users.noreply.github.com>
2023-08-02 10:59:30 +0530

    change reactive methods

--- Part-01/src/main/java/software/ujithamigara/dao/BookDao.java
+++ Part-01/src/main/java/software/ujithamigara/dao/BookDao.java
@@ -4,7 +4,7 @@ import software.ujithamigara.entity.Book;
 
 public interface BookDao {
     public boolean saveBook(Book book);
-    public boolean searchBook(int id);
+    public Book searchBook(int id);
     public boolean updateBook(Book book);
     public boolean deleteBook(Book book);
 }
